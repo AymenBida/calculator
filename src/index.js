20fb579d5c663b513bc72791d1566bf4d6536c6f
AymenBida <aymen.bida.00@gmail.com>
2021-05-20 12:20:16 +0100

    fix linters

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
-//This is a comment just to be able to create a pull request because my changes are already pushed into develop branch
+// This is a comment just to be able to create a pull request
+// because my changes are already pushed into develop branch
