89704969d2906818c986b977cba3a654cab53aa8
AymenBida <aymen.bida.00@gmail.com>
2021-05-20 16:03:41 +0100

    Add Display.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,10 @@
+import Display from './Display';
+// import ButtonPanel from './ButtonPanel';
+
 export default function App() {
   return (
-    <div>Hello World!</div>
+    <>
+      <Display />
+    </>
   );
 }
