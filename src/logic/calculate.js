119b3a8a62380608b85ddcf13fd7007a993987ce
AymenBida <aymen.bida.00@gmail.com>
2021-05-21 20:52:08 +0100

    Made some changements to the modules

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,22 +1,53 @@
 import operate from './operate';
 
+const prepareNumber = (initial, next) => {
+  if (initial === '0' || initial === null) {
+    if (next === '.') return '0.';
+
+    return next;
+  }
+
+  if (initial.includes('.') && next === '.') {
+    return initial;
+  }
+
+  return initial + next;
+};
+
 export default function calculate(calculator = {}, buttonName) {
   const { total, next, operation } = calculator;
-
-  if (buttonName === operation) {
-    return { total: operate(total, next, operation), next: null, operation: null };
-  }
 
   if (buttonName === 'AC') {
     return { total: '0', next: null, operation: null };
   }
 
   if (buttonName === '+/-') {
-    return { total: total * -1, next: next * -1, operation: null };
+    return {
+      total: (total * -1).toString(),
+      next: next ? (next * -1).toString() : null,
+      operation,
+    };
   }
 
-  if (buttonName === '=') {
-    return { total, next: null, operation: null };
+  if (operation === null) {
+    if (['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'].includes(buttonName)) {
+      return { total: prepareNumber(total, buttonName), next: null, operation: null };
+    }
+
+    if (['+', '-', 'x', '%', '÷'].includes(buttonName)) {
+      return { total, next: null, operation: buttonName };
+    }
+  } else {
+    if (['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'].includes(buttonName)) {
+      return { total, next: prepareNumber(next, buttonName), operation };
+    }
+
+    if (['+', '-', 'x', '%', '÷', '='].includes(buttonName)) {
+      if (total && next) {
+        return { total: operate(total, next, operation), next: null, operation: buttonName };
+      }
+      return { total, next: null, operation: buttonName };
+    }
   }
 
   return calculator;
--- src/logic/operate.js
+++ src/logic/operate.js
@@ -5,24 +5,28 @@ export default function operate(numberOne, numberTwo, operation) {
   const n2 = new Big(numberTwo);
 
   if (operation === '+') {
-    return n1 + n2;
+    return n1.plus(n2).toString();
   }
 
   if (operation === '-') {
-    return n1 - n2;
+    return n1.minus(n2).toString();
   }
 
   if (operation === 'x') {
-    return n1 * n2;
+    return n1.times(n2).toString();
   }
 
   if (operation === '÷') {
-    return n1 / n2;
+    if (n2.c.length === 1 && n2.c[0] === 0) {
+      return '0';
+    }
+
+    return n1.div(n2).toString();
   }
 
   if (operation === '%') {
-    return (n1 / 100) * n2;
+    return n1.div(new Big(100)).times(n2).toString();
   }
 
-  return n2;
+  return n2.toString();
 }
