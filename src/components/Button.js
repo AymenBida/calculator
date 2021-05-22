8a23f00891d9f492a68f19dbb859377bd914c17b
AymenBida <aymen.bida.00@gmail.com>
2021-05-22 09:01:00 +0100

    Add event handlers to buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,26 @@
 import PropTypes from 'prop-types';
 import './button.css';
 
-export default function Button(props) {
-  const { name, styling } = props;
+export default function Button({ name, styling, clickHandler }) {
   return (
-    <button type="button" className={styling}>{ name }</button>
+    <button
+      type="button"
+      className={styling}
+      onClick={() => clickHandler(name)}
+      onContextMenu={(e) => {
+        e.preventDefault();
+        clickHandler(name);
+      }}
+    >
+      { name }
+    </button>
   );
 }
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
   styling: PropTypes.string,
+  clickHandler: PropTypes.func.isRequired,
 };
 
 Button.defaultProps = {
