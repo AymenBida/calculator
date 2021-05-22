98d523e7ba0abf1a8407de084a4f840a4d696ffc
AymenBida <aymen.bida.00@gmail.com>
2021-05-22 08:41:48 +0100

    Add Events to the App and Display section

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
+import React from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
-// import calculate from '../logic/calculate';
+import calculate from '../logic/calculate';
 import './app.css';
 
-export default function App() {
-  return (
-    <div className="app">
-      <Display />
-      <ButtonPanel />
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    const newState = calculate(this.state, buttonName);
+    this.setState(newState);
+  }
+
+  render() {
+    const { total, next } = this.state;
+    return (
+      <div className="app">
+        <Display result={next ?? total ?? undefined} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </div>
+    );
+  }
 }
